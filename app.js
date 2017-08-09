6e3b1314f51e4af51c93037352f28c49d654d6aa
Thiago Terleski <thiagoterleski@Mac-mini-de-Thiago.local>
2017-08-09 00:35:21 -0300

    changed way that users are handled, now the state of application is managed by server socket emmiters

--- app.js
+++ app.js
@@ -5,33 +5,91 @@ var io = require('socket.io')(server);
 const port = process.env.PORT || 4001;
 server.listen(port);
 
-clients = {}
+users = []
+
+const removeUser = (id) => {
+  return users.filter(( c ) => c.id !== id);
+}
 
 io.on("connection", function (client) {
   console.log('new connection')
-  client.on("join", function(name){
-    console.log("Joined: " + name);
-    clients[client.id] = name;
+  client.on("join", function(data){
+
+    const alheadExistsClient = users.find(c => c.id === client.id)
+
+    if(alheadExistsClient) {
+      return client.emit("update", {
+        message: "client is alread connected",
+      });
+    }
+
+    const user = {
+      id: client.id,
+      name: data.name,
+      avatar: data.avatar,
+    }
+
+    users.push(user)
+
+    // only user
     client.emit("update", {
       message: "connected",
-      clients: clients,
+      isConnected: true,
+      users: users,
+      isInputModalOpen: false,
+      userName: '',
+      currentUser: user,
     });
+
+    // other users
     client.broadcast.emit("update", {
-      message: "Client "+client.id+" was connected",
-      clients: clients,
+      users: users,
     })
   });
 
   client.on("send", function(msg){
     console.log("Message: " + msg);
-    client.broadcast.emit("chat", clients[client.id], msg);
+    client.broadcast.emit("chat", users[client.id], msg);
   });
 
-  client.on("disconnect", function(){
-    delete clients[client.id];
+  client.on("updateAvatar", function(avatar){
+    const user = users.find(( c ) => c.id == client.id)
+    if (user) {
+      user.avatar = avatar
+      client.emit("update", {
+        message: "avatar changed",
+        users: users,
+      });
+      client.broadcast.emit("update", {
+        users: users,
+      });
+    }
+    //client.broadcast.emit("chat", users[client.id], msg);
+  });
+
+
+  client.on("logout", function(){
+
+    users = removeUser(client.id)
+
     client.broadcast.emit("update", {
-      message: "Client "+client.id+" was connected",
-      clients: clients,
+      message: "User "+client.id+" made logout",
+      users: users,
+    })
+
+    client.emit("update", {
+      message: "disconnected",
+      isConnected: false,
+      users: users,
+    });
+  })
+
+  client.on("disconnect", function(){
+    users = removeUser(client.id)
+    console.log(users)
+    client.broadcast.emit("update", {
+      message: "Client "+client.id+" was disconnected",
+      users: users,
     })
   });
 });
