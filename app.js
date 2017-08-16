7e413e4add6fb6d5d8de4e88767f7d633af22aa9
Thiago Terleski <thiagoterleski@Mac-mini-de-Thiago.local>
2017-08-15 23:54:22 -0300

    refactored and some fixes

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var randomColor = require('randomcolor');
 const port = process.env.PORT || 4001;
 server.listen(port);
 
-users = []
-messageboard = []
+let users = []
+let messageboard = []
+const totalMessages = 10
 
 const removeUser = (id) => {
   return users.filter(( c ) => c.id !== id);
@@ -17,10 +18,20 @@ const getUser = (id) => {
   return users.find(u => u.id === id)
 }
 
+/**
+ * For each new connection, this event will be fired
+ * @param  {client}
+ * A unique identifier for the session, that comes from the underlying Client
+ */
 io.on("connection", function (client) {
 
+  /**
+   * User join to chat
+   * @param  { name: string, avatar: string}
+   */
   client.on("join", function(data){
 
+    // Check if client.id is alhead exists
     const alheadExistsClient = users.find(c => c.id === client.id)
 
     if(alheadExistsClient) {
@@ -29,17 +40,18 @@ io.on("connection", function (client) {
       });
     }
 
+    // Create the new user object and generate a random color for him
     const user = {
       id: client.id,
       name: data.name,
       avatar: data.avatar,
-      color: randomColor({ luminosity: 'bright' }),
+      color: randomColor({ luminosity: 'light' }),
     }
 
     users.push(user)
 
-
-    // only user
+    // sending to the client
+    // The {update} event will be responsible for change the entire state of app
     client.emit("update", {
       message: "connected",
       isConnected: true,
@@ -48,31 +60,28 @@ io.on("connection", function (client) {
       userName: '',
       chat: {
         open: true,
-        messages: messageboard.slice(-10),
+        messages: messageboard.slice(-totalMessages),
       },
       currentUser: user,
     });
 
-    console.log('===============USERS====================')
-    console.log(users)
-
-    // other users
+    // update other users
     client.broadcast.emit("update", {
       users: users,
     })
   });
 
+  /**
+   * Send Message Event
+   * @param  {string} message
+   */
   client.on("sendMessage", function(data){
-    console.info('===================sendMessage=====================')
-    console.log(users)
     const chatUser = getUser(client.id)
-    console.log(client.id)
-    console.log(chatUser)
 
+    // Check if the user is present
     if (!chatUser) {
       return client.emit("error", { message: 'User not found' })
     }
-
 
     messageboard.push({
       message: data.message,
@@ -80,15 +89,21 @@ io.on("connection", function (client) {
       createdAt: new Date().toISOString(),
     })
 
-    const sliceMessages = messageboard.slice(-10)
+    const sliceMessages = messageboard.slice(-totalMessages)
 
+    // Update the client and other users
     client.emit("chat", sliceMessages);
     client.broadcast.emit("chat", sliceMessages);
 
   });
 
+  /**
+   * Change the avatar
+   * @param  {string} avatar name
+   */
   client.on("updateAvatar", function(avatar){
     const user = users.find(( c ) => c.id == client.id)
+
     if (user) {
       user.avatar = avatar
       client.emit("update", {
@@ -99,10 +114,12 @@ io.on("connection", function (client) {
         users: users,
       });
     }
-    //client.broadcast.emit("chat", users[client.id], msg);
   });
 
 
+  /**
+   * Disconect user
+   */
   client.on("logout", function(){
 
     users = removeUser(client.id)
